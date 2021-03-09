cf3956b6381bbc3204f7d483d02f9fdb94b83970
dawolfe2 <79807784+dawolfe2@users.noreply.github.com>
2021-03-09 10:08:20 -0500

    Update GameScript.js

--- HTML-JS Game/GameScript.js
+++ HTML-JS Game/GameScript.js
@@ -1,21 +1,30 @@
 let playerPiece;
+let Boolean = false;
 
 function start() {
+
     GameSpace.start();
+
+        //create game pieces with width, length, starting coordinates and color parameter
     playerPiece = new block(50, 50, 210, 440, "purple");
     yellowPiece = new block(40, 40, 100, 50, "yellow");
     greenPiece = new block(50, 50, 400, 50, "green");
     bluePiece = new block(60, 60, 300, 300, "blue");
+
 }
 
+        //create and update game area
 let GameSpace = {
     canvas : document.createElement("canvas"),
     start : function() {
+
         this.canvas.width = 500;
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(movement, 15);
+
+            //keyboard arrow key controls
         window.addEventListener('keydown', function (e) {
             GameSpace.keys = (GameSpace.keys || []);
             GameSpace.keys[e.keyCode] = true;
@@ -29,36 +38,50 @@ let GameSpace = {
     }
 }
 
+    //create pieces with size, starting position and color parameter
 function block(width, height, xcoord, ycoord, color) {
+
     this.GameSpace = GameSpace;
+
     this.width = width;
     this.height = height;
     this.xcoord = xcoord;
     this.ycoord = ycoord;
+
     this.movex = 0;
     this.movey = 0;
 
+        //update block positions function
     this.move = function(){     
+
         ctx = GameSpace.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.xcoord, this.ycoord, this.width, this.height);
+
     }
+
+        //update player block function
     this.playerMove = function(){
+
         this.xcoord += this.movex;
         this.ycoord += this.movey;
+
     }
 }
 
 function movement(){
+
     GameSpace.clear();
 
     yellowPiece.xcoord = yellowPiece.xcoord + 1;
     greenPiece.ycoord = greenPiece.ycoord + 1;
     bluePiece.ycoord = bluePiece.ycoord - 1;
 
-    playerPiece.movex = 0;
-    playerPiece.movey = 0;
-    
+    if (!Boolean){
+        playerPiece.movex = 0;
+        playerPiece.movey = 0;   
+    }
+
     if (GameSpace.keys && GameSpace.keys[37]){
         playerPiece.movex = -1; 
     }
@@ -73,30 +96,36 @@ function movement(){
     }
 
     playerPiece.playerMove();
+
     playerPiece.move();
-    
     yellowPiece.move();
     greenPiece.move();
     bluePiece.move();
 }
 
 function ButtonUp(){
-    playerPiece.movey -= 1
+    playerPiece.movey -= 1;
+    Boolean = true;
+
 }
 
 function ButtonLeft(){
-    playerPiece.movex -= 1
+    playerPiece.movex -= 1;
+    Boolean = true;
 }
 
 function ButtonRight(){
-    playerPiece.movex += 1
+    playerPiece.movex += 1;
+    Boolean = true;
 }
 
 function ButtonDown(){
-    playerPiece.movey += 1
+    playerPiece.movey += 1;
+    Boolean = true;
 }
 
 function Stop(){
     playerPiece.movex = 0;
     playerPiece.movey = 0;
+    Boolean = false;
 }
